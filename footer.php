0d8b35440b719257b2afe269a6f51a3d87755131
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-05-05 19:29:19 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer>
+    <div class="container">
+        <div class="menu">
+            <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
+        </div>
+        <a href="#" class="logo">Prima<br>Penzion</a>
+        <div class="footerikony">
+            <p class="kontakt">
+                <i class="fas fa-globe-americas"></i>
+                <a href="https://goo.gl/maps/MgsFEvoh9guFecFT6" target="_blank">PrimaPenzion, Jablonského 2, Praha 7</a>
+            </p>
+            <p class="phone">
+                <i class="fas fa-phone"></i>
+                <a href="tel:773348790">773 348 790</a>
+            </p>
+            <p class="mail">
+                <i class="far fa-envelope"></i>
+                info@primapenzion.cz
+            </p>
+        </div>
+        <div class="footermedia">
+            <div class="ikonysoc">
+                <a href="#" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
